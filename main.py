9f461cdac27484bec077e7926ab5710b9baf4d63
Abdullah Abbasi <abdullah.abbasi.0000001@gmail.com>
2026-08-13 22:58:01 +0500

    First commit
    this is the first file of python i which i have learned about the comments,. variables, datatypes, and performed some questions

---
+++ main.py
@@ -0,0 +1,34 @@
+#baiscally hash is used for the comments of single line 
+""" this is a docstring which is used basically for the multiline paragraph"""
+
+#sher ="Abdullah abbasi"
+
+#SheryiansSchool = "students" #pascal case
+
+#sheryiansSchool = "students" #camel case
+#sheriyans_school ='students'#snake case mostly used and mostly good one 
+
+
+name = "abdullah"
+age= 20
+
+#update age
+age=age +1 
+
+print(age)
+print(name)
+
+
+name="Abdullah"
+age=20
+university_name="University of Engineering and Technology , Peshawar"
+Semester=3
+GPA=3.16
+
+print(name, age, university_name, Semester, GPA)
+
+balance =5000
+balance =balance -1250
+balance =balance +2000
+
+print (balance)
